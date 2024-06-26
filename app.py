83e218e9d9235db823c3af30ca0a396a0eab65b8
Pieter Bons <p.c.bons@hva.nl>
2024-06-26 15:49:42 +0200

    make sure words are only sampled once

--- app.py
+++ app.py
@@ -12,15 +12,20 @@ def load_words_from_files(category_files):
             word_dict[category] = words
     return word_dict
 
-# Get next set of words
-def get_next_words(word_dict, categories, words_per_set=5):
+# Get next set of words without repeating until all words are used
+def get_next_words(word_dict, played_words_dict, categories, words_per_set=5):
     selected_words = []
     for category in categories:
-        if len(word_dict[category]) < words_per_set:
-            continue
-        selected_words.extend(word_dict[category][:words_per_set])
-        word_dict[category] = word_dict[category][words_per_set:]
-    random.shuffle(selected_words)
+        available_words = [word for word in word_dict[category] if word not in played_words_dict[category]]
+        if len(available_words) < words_per_set:
+            available_words += played_words_dict[category]
+            played_words_dict[category] = []
+
+        new_words = available_words[:words_per_set]
+        played_words_dict[category].extend(new_words)
+        selected_words.extend(new_words)
+        word_dict[category] = [word for word in word_dict[category] if word not in new_words]
+    
     return selected_words[:words_per_set]
 
 # File paths for each category (adjust paths as necessary)
@@ -69,15 +74,18 @@ if chosen_categories:
         st.session_state.timer = countdown_duration
     if 'words' not in st.session_state:
         st.session_state.words = []
-    if 'round' not in st.session_state:
-        st.session_state.round = 0
+    if 'play_sound_flag' not in st.session_state:
+        st.session_state.play_sound_flag = False
+    if 'played_words_dict' not in st.session_state:
+        st.session_state.played_words_dict = {category: [] for category in category_files.keys()}
+        if uploaded_file is not None:
+            st.session_state.played_words_dict[custom_category_name] = []
 
     # Start/Next button logic
-    if st.button('Start' if st.session_state.round == 0 else 'Next'):
+    if st.button('Next'):
         st.session_state.start = True
         st.session_state.timer = countdown_duration
-        st.session_state.words = get_next_words(word_dict, chosen_categories)
-        st.session_state.round += 1
+        st.session_state.words = get_next_words(word_dict, st.session_state.played_words_dict, chosen_categories)
 
     # Display words immediately
     st.write("Words:")
